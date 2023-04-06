18a5d6e62cc70b35e4acf7aeb91f3ba6779ad573
Zidan Abraham <zidangit07@gmail.com>
2023-04-06 21:11:58 +0800

    add functionality in products.js controllers

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,10 +1,29 @@
 import Product from "../models/product.js";
 
 export const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ company: "ikea" });
+  const products = await Product.find({}).sort("-name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 export const getAllProducts = async (req, res) => {
-  res.status(200).json({ products });
+  const { featured, company, name, sort } = req.query;
+  const query = {};
+  if (featured) {
+    query.featured = featured;
+  }
+  if (company) {
+    query.company = company;
+  }
+  if (name) {
+    query.name = { $regex: name, $options: "i" };
+  }
+  let result = Product.find(query);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
