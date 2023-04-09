b600e271371eb089bf942009c8efb3b8b5b9b2b4
Zidan Abraham <zidangit07@gmail.com>
2023-04-09 15:08:20 +0800

    remove to-do-list, store-api, and JWT

--- 03-to-do-list/middleware/not-found.js
+++
@@ -1,2 +0,0 @@
-const notFound = (req, res) => res.status(404).send("Route Does Not Exist!");
-export default notFound;
--- 05-JWT-Basics/errors/index.js
+++
@@ -1,5 +0,0 @@
-import CustomAPIError from "./custom-error.js";
-import BadRequestError from "./bad-request.js";
-import UnauthenticatedError from "./unauthenticated.js";
-
-export { CustomAPIError, BadRequestError, UnauthenticatedError };
